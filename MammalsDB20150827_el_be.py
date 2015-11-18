02c09b2d8584102606cf049a41fbe11a5bec7af3
chloeshiau <chloe.shiau@gmail.com>
2015-11-18 11:32:10 -0800

    json

--- MammalsDB20150827_el_be.py
+++ MammalsDB20150827_el_be.py
@@ -4,13 +4,14 @@ import MySQLdb
  
 db = MySQLdb.connect(host="localhost", user="root", passwd="ilbaloney.", db="MammalsDB20150827_el_be")
 cursor = db.cursor()
-# cursor.execute("SELECT * FROM rel_animal_article")
+
+#get tables from MySQL
 cursor.execute("SHOW TABLES")
 tables = cursor.fetchall()
 
+#put tables into array
 table_nm = []
 for (table_name,) in cursor:
-    # cursor.execute("SELECT * FROM " + table_name)
     if table_name != 'Paste Errors':
         table_nm.append(table_name)
 
@@ -18,20 +19,25 @@ print table_nm
 
 cursor.close()
 
+#get attributes in each table in database
 for name in table_nm:
 	rowarray_list = []
 	cursor = db.cursor()
-	print ("SELECT * FROM " + name)
 	cursor.execute("SELECT * FROM " + name)
 	rows = cursor.fetchall()
 	columns = [desc[0] for desc in cursor.description]
 	
+	#query the array
 	for row in rows:
 		t = dict(zip(columns, row))
 		rowarray_list.append(t)
 
+	#create json for data
 	j = json.dumps(rowarray_list)
+
+	#put json data into a file
 	rowarrays_file = 'json/'+name + '.json'
 	f = open(rowarrays_file,'w')
 	print >> f, j
+
 	cursor.close()
